18a291f82f0d631b4fe892e1da7346aad850e55e
Kevmorlo <kevinlebeau2004@gmail.com>
2023-05-04 15:32:22 +0200

    ajout des formulaires de connexion et d'inscription

--- static/php/connexion.php
+++ static/php/connexion.php
@@ -8,59 +8,61 @@ include "base.php";
 ?>
 
 <body>
-    <h1>Page de connexion</h1>
     <?php if (isset($error_message)): ?>
     <p><?php echo $error_message; ?></p>
 <?php endif; ?>
-
-<form method="POST">
-    <label for="username">Nom d'utilisateur :</label>
-    <input type="text" id="username" name="username" required><br><br>
-
-    <label for="password">Mot de passe :</label>
-    <input type="password" id="password" name="password" required><br><br>
-
-    <input type="submit" value="Se connecter">
-</form>
+    <div class="form-super-container">
+        <div class="form-container">
+            <form method="POST" class="form">
+                <h1>Se connecter</h1>
+                <input type="text" id="username" name="username" class="form-input" placeholder="Nom d'utilisateur" required>
+                <input type="password" id="password" name="password" class="form-input" placeholder="Mot de passe" required>
+                <input type="submit" value="Se connecter" class="form-button">
+                <a href="inscription.php" class="a-redirect" title="Créer un compte" required>Créer un compte</a>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
 <?php
+// Attendre que le formulaire sois soumis
+if(isset($_POST['username']) && isset($_POST['password'])) {
+    // Vérifier si le formulaire de connexion a été soumis
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Récupérer les informations de connexion soumises par l'utilisateur
+        $username = $_POST["username"];
+        $password = $_POST["password"];
 
-// Vérifier si le formulaire de connexion a été soumis
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Récupérer les informations de connexion soumises par l'utilisateur
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+        // Préparer la requête pour vérifier si l'utilisateur existe dans la base de données
+        $assertion = $conn_bdd->prepare("SELECT id, mot_de_passe FROM utilisateur WHERE nom LIKE :nom");
 
-    // Préparer la requête pour vérifier si l'utilisateur existe dans la base de données
-    $assertion = $conn_bdd->prepare("SELECT id, mot_de_passe FROM utilisateur WHERE nom LIKE :nom");
+        // Exécuter la requête
+        $traitement = $assertion->execute(['nom' => $username]);
 
-    // Exécuter la requête
-    $traitement = $assertion->execute(['nom' => $username]);
-
-    // Afficher le résultat de la requête
-    if($traitement){
-        $resultat = $assertion->fetch();
-    }
-
-    // Vérifier si l'utilisateur existe dans la base de données
-    if ($resultat) {
-        // Vérifier si le mot de passe est correct
-        if ($password == $resultat['mot_de_passe']) {
-
-            // Enregistrer l'identifiant de l'utilisateur dans la session
-            $_SESSION["utilisateur_id"] = $resultat["id"];
-
-            // Rediriger l'utilisateur vers la page d'accueil
-            header("Location: reussi.php");
-            exit();
-        } else {
-            // Afficher un message d'erreur si le mot de passe est incorrect
-            echo('<script>alert("Le mot de passe est incorrect.")</script>)');
+        // Afficher le résultat de la requête
+        if($traitement){
+            $resultat = $assertion->fetch();
         }
-    } else {
-        // Afficher un message d'erreur si l'utilisateur n'existe pas dans la base de données
-        echo('<script>alert("Le nom d\'utilisateur n\'existe pas.")</script>)');
+
+        // Vérifier si l'utilisateur existe dans la base de données
+        if ($resultat) {
+            // Vérifier si le mot de passe est correct
+            if ($password == $resultat['mot_de_passe']) {
+
+                // Enregistrer l'identifiant de l'utilisateur dans la session
+                $_SESSION["utilisateur_id"] = $resultat["id"];
+
+                // Rediriger l'utilisateur vers la page d'accueil
+                header("Location: reussi.php");
+                exit();
+            } else {
+                // Afficher un message d'erreur si le mot de passe est incorrect
+                echo('<script>alert("Le mot de passe est incorrect.")</script>');
+            }
+        } else {
+            // Afficher un message d'erreur si l'utilisateur n'existe pas dans la base de données
+            echo('<script>alert("Le nom d\'utilisateur n\'existe pas.")</script>');
+        }
     }
 }
 
--- static/php/inscription.php
+++ static/php/inscription.php
@@ -1,45 +1,37 @@
+<?php
+// Initialisation
+require 'conn_bdd.php';
+include 'base.php';
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Inscription</title>
-</head>
+?>
 <body>
-	<h2>Inscription</h2>
-	<form action="inscription.php" method="post">
-		<label for="nom">Nom :</label>
-		<input type="text" id="nom" name="nom"><br>
-
-		<label for="prenom">Prénom :</label>
-		<input type="text" id="prenom" name="prenom"><br>
-
-		<label for="email">Email :</label>
-		<input type="email" id="email" name="email"><br>
-
-		<label for="motdepasse">Mot de passe :</label>
-		<input type="password" id="motdepasse" name="motdepasse"><br>
-    
-		<label for="confirmation">Confirmation du mot de passe :</label>
-		<input type="password" id="confirmation" name="confirmation"><br>
-
-		<label for="description">Description :</label>
-		<input type="text" id="prenom" name="prenom"><br>
-
-		<input type="submit" value="S'inscrire">
-	</form>
+	<div class="form-container">
+			<form action="{{ url_for('traitement_register') }}" method="POST" class="form">
+				<h1 class="register-h1">Créer un compte</h1>
+				<input type="text" name="prenom" placeholder="Prénom" class="form-input" id="form-name" required>
+				<input type="text" name="nom" placeholder="Nom" class="form-input" id="form-surname" required>
+				<input type="tel" name="telephone" placeholder="Téléphone" class="form-input" required>
+				<input type="email" name="email" placeholder="Adresse email" class="form-input" required>
+				<input type="password" name="mot_de_passe" placeholder="Mot de passe" class="form-input" required>
+				<input type="password" name="mdp_confirm" placeholder="Confirmation mot de passe" class="form-input" required>
+				<label for="select" class="form-label">Situation :</label>
+				<select name="categorie" placeholder="Situation" class="form-input" id="form-categorie" required>
+					<option value="B1">B1(SN1)</option>
+					<option value="B2">B2(SN2)</option>
+					<option value="B3">B3(CDA/ASRBD/DevOps/IA)</option>
+					<option value="B4">B4(I1)</option>
+					<option value="B5">B5(I2)</option>
+					<option value="Intervenant">Intervenant</option>
+					<option value="Equipe pédagogique">Equipe pédagogique</option>
+					<option value="Autre">Autre</option>
+				</select>
+				<button type="submit" class="form-button">Créer un compte</button>
+				<a href="connexion.php" class="a-redirect" title="Se connecter" required>Se connecter</a>
+			</form>
+		</div>
 </body>
 </html>
 <?php
-// Vérifier si le formulaire a été soumis
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-	// Récupérer les données du formulaire
-	$nom = $_POST['nom'];
-	$prenom = $_POST['prenom'];
-	$email = $_POST['email'];
-	$motdepasse = $_POST['motdepasse'];
-}
-// Connexion à la base de données
-include "conn_bdd.php";
 
 // Code pour insérer les données dans la base de données
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -52,7 +44,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 	// Vérifier que le mot de passe et la confirmation sont identiques
 	if($motdepasse != $confirmation) {
-		echo "Le mot de passe et la confirmation doivent être identiques";
+		echo("<script>alert('Le mot de passe et la confirmation doivent être identiques')</script>");
 	} else {
 		// Hasher le mot de passe avec l'algorithme bcrypt
 		$hash = password_hash($motdepasse, PASSWORD_DEFAULT);
@@ -62,12 +54,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 		// Exécuter la requête SQL
 		if (mysqli_query($conn, $sql)) {
-			echo "Inscription réussie !";
+			echo("<script>alert('Inscription réussie !')</script>");
 		} else {
-			echo "Erreur : " . mysqli_error($conn);
+			echo("<script>alert('Inscription réussie !')</script>");
 		}
 	}
 }
-
-// Fermer la connexion
-mysqli_close($conn);
