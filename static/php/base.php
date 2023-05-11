934f6e8403bb9095296ab82aad47ac7cda2c46c8
Kevmorlo <kevinlebeau2004@gmail.com>
2023-05-11 16:58:23 +0200

    maj index, base, profil, connexion et deconnexion.

--- static/php/base.php
+++ static/php/base.php
@@ -1,23 +1,11 @@
-<?php
-// On définit les valeurs par défaut du favicon et du css
-$favicon = "/static/media/img/icone.ico";
-$css = "/static/css/style.css";
-
-// On vérifie si la page est différente de index.php pour mettre les valeurs correspondantes
-if(basename($_SERVER['PHP_SELF']) != "index.php") {
-    $favicon = "../media/img/icone.ico";
-    $css = "../css/style.css";
-}
-?>
-
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" href="<?= $favicon ?>" type="image/x-icon">
-    <link rel="stylesheet" href="<?= $css ?>">
+    <link rel="shortcut icon" href="/static/media/img/icone.ico" type="image/x-icon">
+    <link rel="stylesheet" href="/static/css/style.css">
     <title>MyProjeception</title>
 </head>
 <body>
@@ -25,7 +13,7 @@ if(basename($_SERVER['PHP_SELF']) != "index.php") {
         <div class="logo">
             <a href="/index.php">
                 <div class="base-button">
-                    <img src="../media/img/mydil.webp" alt="Logo MyDIL" title="MyDIL"></img>
+                    <img src="/static/media/img/mydil.webp" alt="Logo MyDIL" title="MyDIL"></img>
                 </div>
             </a>
         </div>
@@ -33,18 +21,10 @@ if(basename($_SERVER['PHP_SELF']) != "index.php") {
         <?php
         // On vérifie si la page est différente de accueil.php pour afficher le bouton d'accueil
         if(basename($_SERVER['PHP_SELF']) != "accueil.php") {
-            $icone_accueil = "../media/img/icone_accueil.svg";
-            $lien_accueil = "accueil.php";
-            
-            // Si la page est index.php, on ajuste les valeurs
-            if(basename($_SERVER['PHP_SELF']) == "index.php") {
-                $icone_accueil = "static/media/img/icone_accueil.svg";
-                $lien_accueil = "static/php/accueil.php";
-            }
         ?>
-            <a href="<?= $lien_accueil ?>">
+            <a href="/static/php/accueil.php">
                 <div class="base-button">
-                    <img src="<?= $icone_accueil ?>" alt="Accueil" title="Accueil" class="base_icon"></img>
+                    <img src="/static/media/img/icone_accueil.svg" alt="Accueil" title="Accueil" class="base_icon"></img>
                 </div>
             </a>
         <?php
@@ -52,28 +32,18 @@ if(basename($_SERVER['PHP_SELF']) != "index.php") {
 
         // On vérifie si l'utilisateur est connecté
         if(isset($_SESSION['utilisateur_id'])) {
-            $icone_deconnexion = "../media/img/icone_decon.svg";
-            $lien_deconnexion = "deconnexion.php";
         ?>
-            <a href="<?= $lien_deconnexion ?>" class="base-button">
+            <a href="/static/php/deconnexion.php" class="base-button">
                 <div class="base-button">
-                    <img src="<?= $icone_deconnexion ?>" alt="Se déconnecter" title="Se déconnecter" class="base_icon"></img>
+                    <img src="/static/media/img/icone_decon.svg" alt="Se déconnecter" title="Se déconnecter" class="base_icon"></img>
                 </div>
             </a>
         <?php
         } else {
-            $icone_connexion = "../media/img/icone_con.svg";
-            $lien_connexion = "connexion.php";
-            
-            // Si la page est index.php, on ajuste les valeurs
-            if(basename($_SERVER['PHP_SELF']) == "index.php") {
-                $icone_connexion = "static/media/img/icone_con.svg";
-                $lien_connexion = "static/php/connexion.php";
-            }
         ?>
-            <a href="<?= $lien_connexion ?>">
+            <a href="/static/php/connexion.php">
                 <div class="base-button">
-                    <img src="<?= $icone_connexion ?>" alt="Se connecter" title="Se connecter" class="base_icon"></img>
+                    <img src="/static/media/img/icone_con.svg" alt="Se connecter" title="Se connecter" class="base_icon"></img>
                 </div>
             </a>
         <?php
--- static/php/profil.php
+++ static/php/profil.php
@@ -1,3 +1,4 @@
+<body>
 <?php
 // Initialisation
 require 'conn_bdd.php';
@@ -5,49 +6,45 @@ include 'base.php';
 if(!isset($_SESSION)){
     session_start();// Démarrer une session pour l'utilisateur
 }
-	// On prends les données de la bdd si l'utilisateur est connecté sinon on l'envoi vers la page de connexion
-	if(isset($_SESSION["utilisateur_id"])){
-		$id = $_SESSION["utilisateur_id"];
+// On prends les données de la bdd si l'utilisateur est connecté sinon on l'envoi vers la page de connexion
+if(isset($_SESSION["utilisateur_id"])){
+	$id = $_SESSION["utilisateur_id"];
 
-		// On stocke les infos de l'utilisateur dans le tableau donnees_bdd
-		$assertion = $conn_bdd->prepare("SELECT pseudonyme, nom, prenom, telephone, mail, Categorie_id, description FROM utilisateur WHERE id = :id");
-		$assertion->execute(["id" => $id]);
-		$donnees_utilisateur = $assertion->fetch();
-		$assertion = $conn_bdd->prepare("SELECT categorie.id, categorie.nom FROM categorie JOIN utilisateur ON Categorie_id = categorie.id WHERE utilisateur.id = :id;");
-		$assertion->execute(["id" => $id]);
-		$donnees_categorie = $assertion->fetch();
+	// On stocke les infos de l'utilisateur dans le tableau donnees_bdd
+	$assertion = $conn_bdd->prepare("SELECT pseudonyme, nom, prenom, telephone, mail, Categorie_id, description FROM utilisateur WHERE id = :id");
+	$assertion->execute(["id" => $id]);
+	$donnees_utilisateur = $assertion->fetch();
+	$assertion = $conn_bdd->prepare("SELECT categorie.id, categorie.nom FROM categorie JOIN utilisateur ON Categorie_id = categorie.id WHERE utilisateur.id = :id;");
+	$assertion->execute(["id" => $id]);
+	$donnees_categorie = $assertion->fetch();
 
-		// Code pour insérer les données dans la base de données
-	if($_SERVER['REQUEST_METHOD'] == 'POST') {
-		// Récupérer les données du formulaire en les filtrant avec filter_input()
-		$donnees = [
-			'id' => $id,
-			'pseudonyme' => filter_input(INPUT_POST, 'pseudonyme', FILTER_SANITIZE_EMAIL),
-			'nom' => filter_input(INPUT_POST, 'nom', FILTER_UNSAFE_RAW),
-			'prenom' => filter_input(INPUT_POST, 'prenom', FILTER_UNSAFE_RAW),
-			'telephone' => filter_input(INPUT_POST, 'telephone', FILTER_SANITIZE_EMAIL),
-			'mail' => filter_input(INPUT_POST, 'mail', FILTER_VALIDATE_EMAIL),
-			'categorie' => $_POST['categorie'],
-			'description' => filter_input(INPUT_POST, 'description', FILTER_UNSAFE_RAW),
-		];
+	// Récupérer les données du formulaire en les filtrant avec filter_input()
+	$donnees = [
+		'id' => $id,
+		'pseudonyme' => filter_input(INPUT_POST, 'pseudonyme', FILTER_SANITIZE_EMAIL),
+		'nom' => filter_input(INPUT_POST, 'nom', FILTER_UNSAFE_RAW),
+		'prenom' => filter_input(INPUT_POST, 'prenom', FILTER_UNSAFE_RAW),
+		'telephone' => filter_input(INPUT_POST, 'telephone', FILTER_SANITIZE_EMAIL),
+		'mail' => filter_input(INPUT_POST, 'mail', FILTER_VALIDATE_EMAIL),
+		'categorie' => $_POST['categorie'],
+		'description' => filter_input(INPUT_POST, 'description', FILTER_UNSAFE_RAW),
+	];
 
-		// Préparer la requête SQL pour insérer les données dans la base de données
-		$assertion = $conn_bdd->prepare("UPDATE utilisateur SET pseudonyme = :pseudonyme, nom = :nom, prenom = :prenom, telephone = :telephone, mail = :mail, Categorie_id = :categorie, description = :description WHERE id = :id");
-		$assertion->bindParam(':id', $id);
+	// Préparer la requête SQL pour insérer les données dans la base de données
+	$assertion = $conn_bdd->prepare("UPDATE utilisateur SET pseudonyme = :pseudonyme, nom = :nom, prenom = :prenom, telephone = :telephone, mail = :mail, Categorie_id = :categorie, description = :description WHERE id = :id");
+	$assertion->bindParam(':id', $id);
 
-		// Exécuter la requête
-		$traitement = $assertion->execute($donnees);
-		if(!$traitement){
-			echo("Erreur: " . print_r($assertion->errorInfo(), true));
-		}else{
-			echo("<script>alert('Modifications effectuées.')</script>");
-		}
+	// Exécuter la requête
+	$traitement = $assertion->execute($donnees);
+	if(!$traitement){
+		echo("Erreur: " . print_r($assertion->errorInfo(), true));
+	}else{
+		echo("<script>alert('Modifications effectuées.')</script>");
 	}
 }else{
 	header("Location: connexion.php");
 }
 ?>
-<body>
     <div class="form-container">
 		<form action="profil.php" method="POST" class="form">
             <h1 class="register-h1">Mon Profil</h1>
