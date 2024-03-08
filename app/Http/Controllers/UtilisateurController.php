5b198b9b6d6aee47c3bc4bff197ecd37525795e0
Kévin Lebeau <kevin.lebeau-pro@outlook.fr>
2024-03-08 17:06:31 +0100

    maj controlleur + exemple doc

--- app/Http/Controllers/UtilisateurController.php
+++ app/Http/Controllers/UtilisateurController.php
@@ -6,13 +6,26 @@ use Illuminate\Http\Request;
 use App\Enums\ProductStatusEnum;
 use App\Models\Utilisateur;
 
+/**
+ * Gère les actions liées aux utilisateurs
+ * @see Utilisateur
+ */
 class UtilisateurController extends Controller
 {
+    /**
+     * Affiche un utilisateur
+     * @param int $id
+     * @return \Illuminate\View\View
+     */
     public function index(int $id) {
         $utilisateur = Utilisateur::find($id);
         return view('utilisateur.show', ['utilisateur'=>$utilisateur]);
     }
 
+    /**
+     * Affiche tous les utilisateurs
+     * @return \Illuminate\View\View
+     */
     public function showAll() {
         $utilisateur = Utilisateur::all();
         return view('utilisateur.show-all', ['utilisateur'=>$utilisateur]);
--- database/migrations/2024_03_08_151514_create_utilisateurs.php
+++ database/migrations/2024_03_08_151514_create_utilisateurs.php
@@ -4,6 +4,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+/**
+ * Représente la table "utilisateurs" de la base de données
+ * @see Utilisateur
+ */
 class CreateUtilisateursTable extends Migration
 {
     public function up(): void
