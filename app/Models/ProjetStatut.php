9c19bab566791e10a2e6fe17f64fd237ebbe76aa
Kévin Lebeau <kevin.lebeau-pro@outlook.fr>
2024-03-08 16:25:52 +0100

    ajout models et migrations non remplis

---
+++ app/Models/ProjetStatut.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjetStatut extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/ProjetUtilisateur.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjetUtilisateur extends Model
+{
+    use HasFactory;
+}
