a0bf789d8d474a9891ea2f58f9bd2afd5515c3f1
Kévin Lebeau <kevin.lebeau-pro@outlook.fr>
2024-03-25 13:54:34 +0100

    save

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head, Link } from '@inertiajs/vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+<template>
+    <Head title="Index" />
+</template>
