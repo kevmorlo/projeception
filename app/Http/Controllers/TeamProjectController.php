b4b948aa34d004fff6df1132a98f6368ff2aadf9
Kévin Lebeau <kevin.lebeau-pro@outlook.fr>
2024-06-04 11:50:47 +0200

    patch erreur user_id on string

--- app/Http/Controllers/TeamProjectController.php
+++ app/Http/Controllers/TeamProjectController.php
@@ -11,14 +11,15 @@ class TeamProjectController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index($team)
+    public function index($teamId)
     {
+        $team = Team::findOrFail($teamId);
         if (!auth()->user()->hasTeamPermission($team, 'view')) {
             abort(403);
         } else {
             return inertia('Teams/Projects', [
-                'teamProjects' => Team::findOrFail($team)->projects,
-                'teamName' => Team::find($team)->name,
+                'teamProjects' => $team->projects,
+                'teamName' => $team->name,
             ]);
         }
     }
